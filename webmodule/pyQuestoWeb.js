dc28c53b7a726dd00e5394dc664d60af8e86332c
FanchFenua <francois.baldi@etudiant.upf.pf>
2019-07-21 22:01:11 -1000

    Nettoyage du fichier et mise à jour des commentaires

--- webmodule/pyQuestoWeb.js
+++ webmodule/pyQuestoWeb.js
@@ -1,7 +1,9 @@
-function loadUser() {
+// On surveille le chargement d'un fichier
+function checkFile() {
     document.getElementById('toto').addEventListener('change', chargerJSON, false)
 }
 
+// On vérifie qu'il s'agit bien d'un JSON compatible
 function chargerJSON(evt) {
     // On crée un objet fichier à lire
     var reader = new FileReader();
@@ -10,19 +12,7 @@ function chargerJSON(evt) {
         var leJSON = JSON.parse(evt.target.result)
         document.getElementById("result").innerHTML = "<p>propriétaire : "+leJSON.owner+"</p>"
     }
-    // On lit le fichier du champ input qui a l'id files
+    // On lit le fichier du champ input qui a l'id "files"
     reader.readAsText(evt.target.files[0])
 }
 
-/*
-var template, data, html
-    template = "<ul>{{#repo}}<li>{{nom}}</li>{{/repo}}</ul>"
-    data = {
-        "repo": [
-            {"nom" : "Nom A"},
-            {"nom" : "Nom B"},
-            {"nom" : "Nom C"}
-        ]
-    }
-    html = Mustache.render(template,data)
-    document.getElementById("result").innerHTML = html*/
