b25965fb4196775bfd2724a3568789de2e2f924d
FanchFenua <francois.baldi@etudiant.upf.pf>
2019-07-21 22:44:08 -1000

    Gestion d'un json invalide avec affichage d'un message d'erreur

--- webmodule/pyQuestoWeb.js
+++ webmodule/pyQuestoWeb.js
@@ -9,8 +9,13 @@ function chargerJSON(evt) {
     var reader = new FileReader();
     // Quand le fichier est chargé on l'exploite
     reader.onload = function(evt) {
-        var leJSON = JSON.parse(evt.target.result)
-        document.getElementById("result").innerHTML = "<p>propriétaire : "+leJSON.owner+"</p>"
+        try {
+            var leJSON = JSON.parse(evt.target.result)
+        }
+        catch(erreur)  {
+            document.getElementById("erreur").innerHTML = "JSON invalide !<br />"+ erreur
+        }
+        document.getElementById("result").innerHTML = "<p>propriétaire : "+ leJSON.owner +"</p>"
     }
     // On lit le fichier du champ input qui a l'id "files"
     reader.readAsText(evt.target.files[0])
